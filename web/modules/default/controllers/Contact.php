929c7c2d87147bab546de09e58a8062d04d3acb7
mfiari <fiarimike@yahoo.fr>
2016-03-26 19:00:35 +0100

    correction mail contact pro

--- web/modules/default/controllers/Contact.php
+++ web/modules/default/controllers/Contact.php
@@ -164,11 +164,11 @@ class Controller_Contact extends Controller_Default_Template {
 				$messageContent = str_replace("[transport]", $transportContenu, $messageContent);
 				$messageContent = str_replace("[message]", nl2br($message), $messageContent);
 				
-				if (send_mail ("livreur@homemenus.fr", "demande de contact", $messageContent)) {
+				if (send_mail ("livreur@homemenus.fr", "demande de contact d'un livreur", $messageContent)) {
 					
 					$messageContent =  file_get_contents (ROOT_PATH.'mails/confirmation_contact_livreur.html');
 					
-					send_mail ($email, "confirmation demande de contact", $messageContent);
+					send_mail ($email, "confirmation de demande de contact", $messageContent);
 					
 					$request->mailSuccess = true;
 				} else {
@@ -281,68 +281,157 @@ class Controller_Contact extends Controller_Default_Template {
 	
 	public function pro ($request) {
 		if ($request->request_method == "POST") {
+			$errorMessage = array();
 			$reCaptcha = new ReCaptcha(RECAPTCHA_SECRET_KEY);
-			if(isset($_POST["g-recaptcha-response"])) {
+			if(isset($_POST["g-recaptcha-response"]) && $_POST["g-recaptcha-response"] != '') {
 				$resp = $reCaptcha->verifyResponse(
 					$_SERVER["REMOTE_ADDR"],
 					$_POST["g-recaptcha-response"]	
 				);
-				if ($resp != null && $resp->success) {
-					echo "OK";
-				} else {
-					echo "CAPTCHA incorrect";
+				if ($resp == null || !$resp->success) {
+					$errorMessage["ERROR_CAPTCHA"] = "Une erreur est survenu, veuillez réessayer";
 				}
+			} else {
+				$errorMessage["NO_CAPTCHA"] = "Veuillez valider le reCAPTCHA afin de prouvez que vous n'êtes pas un robot";
 			}
 			$categorie = $_POST['categorie'];
 			if ($categorie == "entreprise") {
-				$restaurant = $_POST['entreprise'];
-				$code_postal = $_POST['code_postal'];
-				$ville = $_POST['ville'];
-				
-				$nom = $_POST['nom'];
-				$prenom = $_POST['prenom'];
-				$telephone = $_POST['telephone'];
-				$fonction = $_POST['fonction'];
-				$message = $_POST['message'];
-				
-				$messageContent =  file_get_contents (ROOT_PATH.'mails/contact_pro_entreprise.html');
-				
-				$messageContent = str_replace("[restaurant]", $restaurant, $messageContent);
-				$messageContent = str_replace("[ville]", $ville, $messageContent);
-				$messageContent = str_replace("[code_postal]", $code_postal, $messageContent);
-				$messageContent = str_replace("[nom]", $nom, $messageContent);
-				$messageContent = str_replace("[prenom]", $prenom, $messageContent);
-				$messageContent = str_replace("[telephone]", $telephone, $messageContent);
-				$messageContent = str_replace("[fonction]", $fonction, $messageContent);
-				$messageContent = str_replace("[message]", nl2br($message), $messageContent);
-				
-				if (send_mail ("restaurant@homemenus.fr", "demande de contact d'une entreprise", $messageContent)) {
-					$request->mailSuccess = true;
+				if (!isset($_POST["entreprise"]) || trim($_POST["entreprise"]) == "") {
+					$errorMessage["EMPTY_RESTAURANT"] = "Veuillez renseigner le nom de votre entreprise";
 				} else {
-					$request->mailSuccess = false;
+					$entreprise = $_POST["entreprise"];
+				}
+				if (!isset($_POST["code_postal"]) || trim($_POST["code_postal"]) == "") {
+					$errorMessage["EMPTY_CP"] = "Veuillez renseigner le code postal de votre entreprise";
+				} else {
+					$code_postal = $_POST["code_postal"];
+				}
+				if (!isset($_POST["ville"]) || trim($_POST["ville"]) == "") {
+					$errorMessage["EMPTY_VILLE"] = "Veuillez renseigner la ville de votre entreprise";
+				} else {
+					$ville = $_POST["ville"];
+				}
+				if (!isset($_POST["nom"]) || trim($_POST["nom"]) == "") {
+					$errorMessage["EMPTY_NOM"] = "Veuillez renseigner votre nom";
+				} else {
+					$nom = $_POST["nom"];
+				}
+				if (!isset($_POST["prenom"]) || trim($_POST["prenom"]) == "") {
+					$errorMessage["EMPTY_PRENOM"] = "Veuillez renseigner votre prénom";
+				} else {
+					$prenom = $_POST["prenom"];
+				}
+				if (!isset($_POST["telephone"]) || trim($_POST["telephone"]) == "") {
+					$errorMessage["EMPTY_TEL"] = "Veuillez renseigner votre telephone";
+				} else {
+					$telephone = $_POST["telephone"];
+				}
+				if (!isset($_POST["email"]) || trim($_POST["email"]) == "") {
+					$errorMessage["EMPTY_EMAIL"] = "Veuillez renseigner votre email";
+				} else {
+					$email = $_POST["email"];
+				}
+				if (!isset($_POST["fonction"]) || trim($_POST["fonction"]) == "") {
+					$errorMessage["EMPTY_FONCTION"] = "Veuillez renseigner votre fonction";
+				} else {
+					$fonction = $_POST["fonction"];
+				}
+				if (!isset($_POST["message"]) || trim($_POST["message"]) == "") {
+					$message = '';
+				} else {
+					$message = $_POST['message'];
+				}
+				
+				if (count($errorMessage) == 0) {
+				
+					$messageContent =  file_get_contents (ROOT_PATH.'mails/contact_pro_entreprise.html');
+					
+					$messageContent = str_replace("[entreprise]", $entreprise, $messageContent);
+					$messageContent = str_replace("[ville]", $ville, $messageContent);
+					$messageContent = str_replace("[code_postal]", $code_postal, $messageContent);
+					$messageContent = str_replace("[nom]", $nom, $messageContent);
+					$messageContent = str_replace("[prenom]", $prenom, $messageContent);
+					$messageContent = str_replace("[telephone]", $telephone, $messageContent);
+					$messageContent = str_replace("[email]", $email, $messageContent);
+					$messageContent = str_replace("[fonction]", $fonction, $messageContent);
+					$messageContent = str_replace("[message]", nl2br($message), $messageContent);
+					
+					if (send_mail ("pro@homemenus.fr", "demande de contact d'une entreprise", $messageContent)) {
+						
+						$messageContent =  file_get_contents (ROOT_PATH.'mails/confirmation_contact_livreur.html');
+					
+						send_mail ($email, "confirmation de demande de contact", $messageContent);
+						
+						$request->mailSuccess = true;
+					} else {
+						$request->mailSuccess = false;
+					}
+				} else {
+					$request->errorMessage = $errorMessage;
+					$request->fieldEntreprise = $entreprise;
+					$request->fieldCP = $code_postal;
+					$request->fieldVille = $ville;
+					$request->fieldNom = $nom;
+					$request->fieldPrenom = $prenom;
+					$request->fieldTelephone = $telephone;
+					$request->fieldEmail = $email;
+					$request->fieldFonction = $fonction;
+					$request->fieldMessage = $message;
 				}
 			} else {
-				$nom = $_POST['nom'];
-				$prenom = $_POST['prenom'];
-				$telephone = $_POST['telephone'];
-				$fonction = $_POST['fonction'];
-				$message = $_POST['message'];
-				
-				$messageContent =  file_get_contents (ROOT_PATH.'mails/contact_pro_particulier.html');
-				
-				$messageContent = str_replace("[restaurant]", $restaurant, $messageContent);
-				$messageContent = str_replace("[ville]", $ville, $messageContent);
-				$messageContent = str_replace("[code_postal]", $code_postal, $messageContent);
-				$messageContent = str_replace("[nom]", $nom, $messageContent);
-				$messageContent = str_replace("[prenom]", $prenom, $messageContent);
-				$messageContent = str_replace("[telephone]", $telephone, $messageContent);
-				$messageContent = str_replace("[fonction]", $fonction, $messageContent);
-				$messageContent = str_replace("[message]", nl2br($message), $messageContent);
-				
-				if (send_mail ("restaurant@homemenus.fr", "demande de contact d'un particulier", $messageContent)) {
-					$request->mailSuccess = true;
+				if (!isset($_POST["nom"]) || trim($_POST["nom"]) == "") {
+					$errorMessage["EMPTY_NOM"] = "Veuillez renseigner votre nom";
 				} else {
-					$request->mailSuccess = false;
+					$nom = $_POST["nom"];
+				}
+				if (!isset($_POST["prenom"]) || trim($_POST["prenom"]) == "") {
+					$errorMessage["EMPTY_PRENOM"] = "Veuillez renseigner votre prénom";
+				} else {
+					$prenom = $_POST["prenom"];
+				}
+				if (!isset($_POST["telephone"]) || trim($_POST["telephone"]) == "") {
+					$errorMessage["EMPTY_TEL"] = "Veuillez renseigner votre telephone";
+				} else {
+					$telephone = $_POST["telephone"];
+				}
+				if (!isset($_POST["email"]) || trim($_POST["email"]) == "") {
+					$errorMessage["EMPTY_EMAIL"] = "Veuillez renseigner votre email";
+				} else {
+					$email = $_POST["email"];
+				}
+				if (!isset($_POST["message"]) || trim($_POST["message"]) == "") {
+					$message = '';
+				} else {
+					$message = $_POST['message'];
+				}
+				
+				if (count($errorMessage) == 0) {
+				
+					$messageContent =  file_get_contents (ROOT_PATH.'mails/contact_pro_particulier.html');
+					
+					$messageContent = str_replace("[nom]", $nom, $messageContent);
+					$messageContent = str_replace("[prenom]", $prenom, $messageContent);
+					$messageContent = str_replace("[telephone]", $telephone, $messageContent);
+					$messageContent = str_replace("[email]", $email, $messageContent);
+					$messageContent = str_replace("[message]", nl2br($message), $messageContent);
+					
+					if (send_mail ("pro@homemenus.fr", "demande de contact d'un particulier", $messageContent)) {
+						
+						$messageContent =  file_get_contents (ROOT_PATH.'mails/confirmation_contact_livreur.html');
+					
+						send_mail ($email, "confirmation de demande de contact", $messageContent);
+						
+						$request->mailSuccess = true;
+					} else {
+						$request->mailSuccess = false;
+					}
+				} else {
+					$request->errorMessage = $errorMessage;
+					$request->fieldNom = $nom;
+					$request->fieldPrenom = $prenom;
+					$request->fieldTelephone = $telephone;
+					$request->fieldEmail = $email;
+					$request->fieldMessage = $message;
 				}
 			}
 		}
--- web/modules/default/vues/contact/pro.php
+++ web/modules/default/vues/contact/pro.php
@@ -1,20 +1,29 @@
 <h2>Contact</h2>
+<?php if ($request->errorMessage) : ?>
+	<?php foreach ($request->errorMessage as $key => $value) : ?>
+		<div class="alert alert-danger" role="alert">
+			<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+			<span class="sr-only">Error:</span>
+			<?php echo $value; ?>
+		</div>
+	<?php endforeach; ?>
+<?php endif; ?>
+<?php if ($request->hasProperty("mailSuccess")) : ?>
+	<?php if ($request->mailSuccess === true) : ?>
+		<div class="alert alert-success" role="alert">
+			<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+			Votre mail a bien été transmis aux équipe de homemenus
+		</div>
+	<?php else : ?>
+		<div class="alert alert-danger" role="alert">
+			<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+			<span class="sr-only">Error:</span>
+			Une erreur est survenu lors de l'envoi du mail, veuillez réessayer
+		</div>
+	<?php endif; ?>
+<?php endif; ?>
 <div class="row">
 	<div class="col-md-10 col-md-offset-1">
-		<?php if ($request->hasProperty("mailSuccess")) : ?>
-			<?php if ($request->mailSuccess === true) : ?>
-				<div class="alert alert-success" role="alert">
-					<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
-					Votre mail a bien été transmis aux équipe de cservichezvous
-				</div>
-			<?php else : ?>
-				<div class="alert alert-danger" role="alert">
-					<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-					<span class="sr-only">Error:</span>
-					Une erreur est survenu lors de l'envoi du mail, veuillez réessayer
-				</div>
-			<?php endif; ?>
-		<?php endif; ?>
 		<form method="post" enctype="x-www-form-urlencoded" id="contactForm" action="">
 			<fieldset>
 				<div class="form-group">
@@ -28,38 +37,42 @@
 					<h3>Votre entreprise</h3>
 					<div class="form-group">
 						<label for="entreprise">Nom<span class="required">*</span> : </label>
-						<input class="form-control" name="entreprise" type="text" required>
+						<input class="form-control" name="entreprise" type="text" value="<?php echo $request->fieldEntreprise !== false ? $request->fieldEntreprise : ''; ?>" required>
 					</div>
 					<div class="form-group">
 						<label for="code_postal">Code postal<span class="required">*</span> : </label>
-						<input class="form-control" name="code_postal" type="text" required>
+						<input class="form-control" name="code_postal" type="text" value="<?php echo $request->fieldCP !== false ? $request->fieldCP : ''; ?>" required>
 					</div>
 					<div class="form-group">
 						<label for="ville">Ville<span class="required">*</span> : </label>
-						<input class="form-control" name="ville" type="text" required>
+						<input class="form-control" name="ville" type="text" value="<?php echo $request->fieldVille !== false ? $request->fieldVille : ''; ?>" required>
 					</div>
 				</div>
 				<h3>Vos coordonnées</h3>
 				<div class="form-group">
 					<label for="nom">Nom<span class="required">*</span> : </label>
-					<input class="form-control" name="nom" type="text" required>
+					<input class="form-control" name="nom" type="text" value="<?php echo $request->fieldNom !== false ? $request->fieldNom : ''; ?>" required>
 				</div>
 				<div class="form-group">
 					<label for="prenom">Prenom<span class="required">*</span> : </label>
-					<input class="form-control" name="prenom" type="text" required>
+					<input class="form-control" name="prenom" type="text" value="<?php echo $request->fieldPrenom !== false ? $request->fieldPrenom : ''; ?>" required>
 				</div>
 				<div class="form-group">
 					<label for="telephone">Téléphone<span class="required">*</span> : </label>
-					<input class="form-control" name="telephone" type="text" required>
+					<input class="form-control" name="telephone" type="text" value="<?php echo $request->fieldTelephone !== false ? $request->fieldTelephone : ''; ?>" required>
 				</div>
 				<div class="form-group">
+					<label for="email">Votre email<span class="required">*</span> : </label>
+					<input class="form-control" name="email" type="email" value="<?php echo $request->fieldEmail !== false ? $request->fieldEmail : ''; ?>" required>
+				</div>
+				<div id="divFieldFonction" class="form-group">
 					<label for="fonction">Fonction<span class="required">*</span> : </label>
-					<input class="form-control" name="fonction" type="text" required>
+					<input class="form-control" name="fonction" type="text" value="<?php echo $request->fieldFonction !== false ? $request->fieldFonction : ''; ?>" required>
 				</div>
 				<h3>Informations complémentaires</h3>
 				<div class="form-group">
 					<label for="message">Message : </label>
-					<textarea class="form-control" name="message" rows="8" cols="45" required></textarea>
+					<textarea class="form-control" name="message" rows="8" cols="45"><?php echo $request->fieldMessage !== false ? $request->fieldMessage : ''; ?></textarea>
 				</div>
 				<div class="g-recaptcha" data-sitekey="<?php echo RECAPTCHA_SITE_KEY; ?>"></div>
 				<input name="status" type="text" hidden="hidden" value="user"/>
@@ -71,12 +84,15 @@
 <script type="text/javascript">
 	$(function () {
 		$("#entreprise").hide();
+		$("#divFieldFonction").hide();
 		$("#categorie").change(function() {
 			var value = $(this).val();
 			if (value == "entreprise") {
 				$("#entreprise").show();
+				$("#divFieldFonction").show();
 			} else {
 				$("#entreprise").hide();
+				$("#divFieldFonction").hide();
 			}
 		});
 	});
