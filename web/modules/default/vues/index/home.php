1189ba61803beca91ace05d8f70a5672961da0b5
mfiari <fiarimike@yahoo.fr>
2016-08-10 21:59:13 +0200

    ajout slider new page accueil

--- web/modules/default/vues/index/home.php
+++ web/modules/default/vues/index/home.php
@@ -1,46 +1,45 @@
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-	<!-- Indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-		<li data-target="#myCarousel" data-slide-to="2"></li>
-	</ol>
+<div class="row">
+	<div class="col-md-10  col-md-offset-1">
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+			</ol>
 
-	<!-- Wrapper for slides -->
-	<div class="carousel-inner" role="listbox">
-		<div class="item active">
-			<img src="res/img/slider1.jpg" alt="HoMe Menus - Livraison">
-			<div class="carousel-caption">
-				<h3>Livraison Gratuite</h3>
-				<p>Livraison gratuite dimanche 10 juillet a partir de 19 heures.</p>
+			<!-- Wrapper for slides -->
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+					<a href="?action=restaurant_partenaire&id=10">
+						<img style="height : 300px; width : 100%;" src="res/img/slider1.jpg" title="HoMe Menus - La villa Mantes la jolie" alt="HoMe Menus - La villa Mantes la jolie">
+						<div class="carousel-caption">
+							<h3>Ouverture de La Villa</h3>
+							<p>Ouverture du restaurant La Villa à Mantes la jolie. Commandez dès maintenant.</p>
+						</div>
+					</a>
+				</div>
+				<div class="item">
+					<a href="?controler=contact&action=livreur">
+						<img style="height : 300px; width : 100%;" src="res/img/slider2.jpg" title="HoMe Menus - Livreur" alt="HoMe Menus - Livreur">
+						<div class="carousel-caption">
+							<h3>Recrutement livreur</h3>
+							<p>Nous cherchons des livreurs pour le mois de septembre, disponible de 11h à 14h ! Contactez-nous dès maintenant.</p>
+						</div>
+					</a>
+				</div>
 			</div>
-		</div>
-		<div class="item">
-			<img src="res/img/slider1.jpg" alt="HoMe Menus - Livraison">
-			<div class="carousel-caption">
-				<h3>Précommande disponible</h3>
-				<p>Nous ne livrons qu’à partir de 19h mais vous pouvez précommander dès maintenant.</p>
-			</div>
-		</div>
 
-		<div class="item">
-			<img src="res/img/slider1.jpg" alt="HoMe Menus - Livraison">
-			<div class="carousel-caption">
-				<h3>HoMe Menus</h3>
-				<p></p>
-			</div>
+			<!-- Left and right controls -->
+			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
 		</div>
 	</div>
-
-	<!-- Left and right controls -->
-	<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-		<span class="sr-only">Previous</span>
-	</a>
-	<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-	</a>
 </div>
 <div class="col-md-12 even">
 	<h2>HoMe Menus</h2>
