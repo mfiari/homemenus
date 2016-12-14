5c87de70b103a8c49bd950fe04b43cbd6634c9f9
mfiari <fiarimike@yahoo.fr>
2016-12-14 07:48:54 +0100

    ajout facture client admin

--- models/PDF.php
+++ models/PDF.php
@@ -162,6 +162,104 @@ class PDF extends FPDF {
 		$this->Cell($width,10,$totalPrix.' '.chr(128),0,1);
 	}
 	
+	public function generateFactureClientAdmin ($commande) {
+		
+		$this->title = "Commande #".$commande->id;
+		
+		$this->AliasNbPages();
+		$this->AddPage();
+		
+		$width = 70;
+		// Police Arial gras 12
+		$this->SetFont('Arial','B',12);
+		
+		$this->Cell($width,10,'Client',0,0);
+		$this->Cell($width,10,'',0,0);
+		$this->Cell($width,10,utf8_encode($commande->client->nom).' '.utf8_encode($commande->client->prenom),0,1);
+		
+		$this->Cell($width,10,'Restaurant',0,0);
+		$this->Cell($width,10,'',0,0);
+		$this->Cell($width,10,utf8_encode($commande->restaurant->nom),0,1);
+		
+		$this->Cell($width,10,'Date de commande',0,0);
+		$this->Cell($width,10,'',0,0);
+		$this->Cell($width,10,formatTimestampToDateHeure($commande->date_commande),0,1);
+		
+		// Saut de ligne
+		$this->Ln(20);
+		
+		$width = 80;
+		$this->SetFont('Times','',12);
+		
+		$totalPrix = 0;
+		$totalQte = 0;
+		foreach ($commande->menus as $menu) {
+			$name = utf8_encode($menu->nom);
+			if (count($menu->formats) == 1 && $menu->formats[0]->nom != "") {
+				$name .= ' ('.utf8_encode($menu->formats[0]->nom).')';
+			}
+			$this->Cell($width,10,$name,0,0);
+			$this->Cell($width,10,' X '.$menu->quantite,0,0);
+			$this->Cell($width,10,$menu->prix.' '.chr(128),0,1);
+			foreach ($menu->formules as $formule) {
+				$this->Cell(0,10,utf8_encode($formule->nom),0,1);
+				foreach ($formule->categories as $categorie) {
+					$this->Cell(0,10,utf8_encode($categorie->nom),0,1);
+					foreach ($categorie->contenus as $contenu) {
+						$this->Cell(0,10,utf8_encode($contenu->nom),0,1);
+					}
+				}
+			}
+			$totalQte += $menu->quantite;
+			$totalPrix += $menu->prix;
+			$this->Cell(0,5,'','B',1);
+		}
+		foreach ($commande->cartes as $carte) {
+			$name = utf8_encode($carte->nom);
+			if (count($carte->formats) == 1 && $carte->formats[0]->nom != "") {
+				$name .= ' ('.utf8_encode($carte->formats[0]->nom).')';
+			}
+			$this->Cell($width,10,$name,0,0);
+			$this->Cell($width,10,' X '.$carte->quantite,0,0);
+			$this->Cell($width,10,$carte->prix.' '.chr(128),0,1);
+			if (count($carte->supplements) > 0) {
+				$this->Cell(0,10,'Suppléments : ',0,1);
+				foreach ($carte->supplements as $supplement) {
+					$this->Cell(0,10,$supplement->nom,0,1);
+				}
+			}
+			if (count($carte->options) > 0) {
+				foreach ($carte->options as $option) {
+					foreach ($option->values as $value) {
+						$this->Cell(20,10,'',0,0);
+						$this->Cell(0,10,utf8_encode($option->nom).' : '.utf8_encode($value->nom),0,1);
+					}
+				}
+			}
+			if (count($carte->accompagnements) > 0) {
+				$this->Cell(20,10,'',0,0);
+				$this->Cell(0,10,utf8_encode('accompagnements : '),0,1);
+				foreach ($carte->accompagnements as $accompagnement) {
+					foreach ($accompagnement->cartes as $carteAccompagnement) {
+						$this->Cell(40,10,'',0,0);
+						$this->Cell(0,10,$carteAccompagnement->nom,0,1);
+					}
+				}
+			}
+			$this->Cell(0,5,'','B',1);
+			$totalQte += $carte->quantite;
+			$totalPrix += $carte->prix;
+		}
+		$this->Cell($width,10,'Prix de livraison :',0,0);
+		$this->Cell($width,10,'',0,0);
+		$this->Cell($width,10,$commande->prix_livraison.' '.chr(128),0,1);
+		$totalPrix += $commande->prix_livraison;
+		$this->Cell(0,5,'','B',1);
+		$this->Cell($width,10,'Total :',0,0);
+		$this->Cell($width,10,$totalQte,0,0);
+		$this->Cell($width,10,$totalPrix.' '.chr(128),0,1);
+	}
+	
 	public function generateFactureRestaurant ($commande) {
 		
 		$this->title = "Commande #".$commande->id;
