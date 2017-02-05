3377e9f63b4e5471c6b8d38f67ba915c288c2147
mfiari <fiarimike@yahoo.fr>
2017-02-05 14:21:50 +0100

    ajout panier et logout header

--- web/layouts/top.php
+++ web/layouts/top.php
@@ -12,7 +12,20 @@
 			<ul class="menu">
 				<li><a href="contact.html">Contact</a></li>
 				<li><a href="faq.html">FAQ</a></li>
-				<li class="user"><a href="contact.html"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a></li>
+				<?php if ($request->_auth) : ?>
+					<li class="user"><a href="?controler=compte"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a></li>
+				<?php else : ?>
+					<li class="user"><a data-toggle="modal" data-target="#login-modal"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a></li>
+				<?php endif; ?>
+				<li style="color : #FF2A00;"><a id="cardMenu">
+					<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+					<?php if ($request->_itemsPanier && $request->_itemsPanier > 0) : ?>
+						<span class="badge"><?php echo $request->_itemsPanier; ?></span>
+					<?php endif; ?>
+				</a></li>
+				<?php if ($request->_auth) : ?>
+					<li style="color : #FF2A00;"><a href="index.php?action=logout"><span class="glyphicon glyphicon-off" aria-hidden="true"></span></a></li>
+				<?php endif; ?>
 			</ul>
 		</div>
 	</div>
