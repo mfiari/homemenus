5c6158e101b816da8289183fd6666fee2a731938
mfiari <fiarimike@yahoo.fr>
2017-02-05 13:43:48 +0100

    ajout handler fatal error

--- function.php
+++ function.php
@@ -154,6 +154,57 @@ function writeLog ($type, $texte, $level = LOG_LEVEL_INFO, $message = null) {
 	}
 }
 
+function fatal_error_handler () {
+	$errfile = "unknown file";
+	$errstr  = "shutdown";
+	$errno   = E_CORE_ERROR;
+	$errline = 0;
+
+	$error = error_get_last();
+
+	if ($error !== NULL) {
+		$errno   = $error["type"];
+		$errfile = $error["file"];
+		$errline = $error["line"];
+		$errstr  = $error["message"];
+		$mailMessage = format_error( $errno, $errstr, $errfile, $errline);
+		send_mail ("informatique@homemenus.fr", "Fatal error", $mailMessage);
+	}
+}
+
+function format_error ($errno, $errstr, $errfile, $errline) {
+	$trace = print_r( debug_backtrace( false ), true );
+
+	$content = "
+		<table>
+			<thead><th>Item</th><th>Description</th></thead>
+			<tbody>
+				<tr>
+					<th>Error</th>
+					<td><pre>$errstr</pre></td>
+				</tr>
+				<tr>
+					<th>Errno</th>
+					<td><pre>$errno</pre></td>
+				</tr>
+				<tr>
+					<th>File</th>
+					<td>$errfile</td>
+				</tr>
+				<tr>
+					<th>Line</th>
+					<td>$errline</td>
+				</tr>
+				<tr>
+					<th>Trace</th>
+					<td><pre>$trace</pre></td>
+				</tr>
+			</tbody>
+		</table>";
+		
+	return $content;
+}
+
 function send_mail ($to, $subject, $message, $from = MAIL_FROM_DEFAULT, $attachments = array()) {
 	require_once WEBSITE_PATH.'res/lib/phpmailer/class.phpmailer.php';
 	require_once MODEL_PATH.'Template.php';
