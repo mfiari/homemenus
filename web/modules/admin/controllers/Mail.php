d211db82fc51df5c226a92aa23246816520220d0
mfiari <fiarimike@yahoo.fr>
2017-02-13 23:41:38 +0100

    ajout propriete request controller admin mail

--- web/modules/admin/controllers/Mail.php
+++ web/modules/admin/controllers/Mail.php
@@ -5,6 +5,7 @@ include_once ROOT_PATH."models/Mail.php";
 class Controller_Mail extends Controller_Admin_Template {
 	
 	public function manage ($request) {
+		$this->request = $request;
 		if (isset($_GET["action"])) {
 			$action = $_GET["action"];
 			switch ($action) {
