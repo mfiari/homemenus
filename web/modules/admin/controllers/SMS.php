f26910ef2f9bf311f135dd047d49275c18662633
mfiari <fiarimike@yahoo.fr>
2017-02-14 07:33:57 +0100

    ajout propriete request controller admin sms

--- web/modules/admin/controllers/SMS.php
+++ web/modules/admin/controllers/SMS.php
@@ -6,6 +6,7 @@ include_once ROOT_PATH."models/Clickatell.php";
 class Controller_SMS extends Controller_Admin_Template {
 	
 	public function manage ($request) {
+		$this->request = $request;
 		if (isset($_GET["action"])) {
 			$action = $_GET["action"];
 			switch ($action) {
